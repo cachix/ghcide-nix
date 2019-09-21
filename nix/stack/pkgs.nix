0237d173c67923fdbd63f70b7b923d83324c74bf
Domen Kožar <domen@dev.si>
2019-09-21 19:02:31 +0200

    0.0.3

--- nix/stack/pkgs.nix
+++ nix/stack/pkgs.nix
@@ -1,13 +1,12 @@
 {
   extras = hackage:
     {
-      packages = {} // {
-        ghcide = ./ghcide.nix;
-        haskell-lsp = ./haskell-lsp.nix;
-        haskell-lsp-types = ./haskell-lsp-types.nix;
-        lsp-test = ./lsp-test.nix;
-        hie-bios = ./hie-bios.nix;
-        };
+      packages = ({
+        "haskell-lsp" = (((hackage.haskell-lsp)."0.16.0.0").revisions).default;
+        "haskell-lsp-types" = (((hackage.haskell-lsp-types)."0.16.0.0").revisions).default;
+        "lsp-test" = (((hackage.lsp-test)."0.7.0.0").revisions).default;
+        "hie-bios" = (((hackage.hie-bios)."0.2.0").revisions).default;
+        } // { ghcide = ./ghcide.nix; }) // {};
       };
   resolver = "nightly-2019-09-16";
   }
