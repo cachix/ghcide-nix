c1a04a670c9ce62f4bd67751c5a7a621028d64d3
Robert Hensing <robert@roberthensing.nl>
2019-11-27 13:57:30 +0100

    Format

--- nix/default.nix
+++ nix/default.nix
@@ -13,9 +13,10 @@ let
                 ghc.package = ghc; 
                 compiler.version = pkgs.lib.mkForce ghc.version;
                 reinstallableLibGhc = true;
-                nonReinstallablePkgs = ["ghc-boot" "binary" "process" "bytestring" "containers" "directory" 
-                   "filepath" "hpc" "ghci" "terminfo" "time" "transformers" "unix" "text" "base"]
-                ++ pkgs.lib.optionals (ghc.version == "8.8.1") [ "contravariant" ];
+                nonReinstallablePkgs =
+                  ["ghc-boot" "binary" "process" "bytestring" "containers" "directory"
+                   "filepath" "hpc" "ghci" "terminfo" "time" "transformers" "unix" "text" "base"
+                  ] ++ pkgs.lib.optionals (ghc.version == "8.8.1") [ "contravariant" ];
             }];
           };
         in pkgSet.config.hsPkgs;
